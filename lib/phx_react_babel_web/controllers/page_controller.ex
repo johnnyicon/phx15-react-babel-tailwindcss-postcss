f712be43080dfb71ba6dea05beae8e393b4281cd
John Gonzales <john@merkhetventures.com>
2021-05-13 12:39:20 -0400

    Setup React

--- lib/phx_react_babel_web/controllers/page_controller.ex
+++ lib/phx_react_babel_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule PhxReactBabelWeb.PageController do
   use PhxReactBabelWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", props: Jason.encode!(%{name: "bar"}))
   end
 end
